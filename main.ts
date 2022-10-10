77b4871bde3b48671f747407d0ba2fecb74e0f04
6a0 <87043474+6a0@users.noreply.github.com>
2022-10-10 20:42:37 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -51,88 +51,90 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 radio.onReceivedString(function (receivedString) {
-    if (receivedString == "connected") {
-        basic.showIcon(IconNames.Yes)
-        basic.pause(500)
-        basic.showLeds(`
-            . . . . .
-            # # # # #
-            # . . . #
-            # # # # #
-            . . . # .
-            `)
-        music.playMelody("E G B C5 - - - - ", 165)
-        connectindicator = 1
-    } else if (receivedString == "connect") {
-        disableinterferencecheck = 1
-        radio.sendString("connected")
-        basic.showIcon(IconNames.Yes)
-        basic.pause(1000)
-        basic.showLeds(`
-            . . . . .
-            # # # # #
-            # . . . #
-            # # # # #
-            . . . # .
-            `)
-        music.playMelody("E G B C5 - - - - ", 165)
-        connectindicator = 1
-    } else if (connectindicator == 1) {
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            . . # . .
-            . . . . .
-            . . # . .
-            `)
-        music.playMelody("G A C5 - - - - - ", 190)
-        basic.pause(500)
-        if (receivedString == "O1Vt55XkxF") {
-            basic.showIcon(IconNames.Heart)
-        } else if (receivedString == "2iexReSuMD") {
-            basic.showIcon(IconNames.Happy)
-        } else if (receivedString == "aH8rn9PxmT") {
-            basic.showIcon(IconNames.Sad)
-        } else if (receivedString == "fKk4YXjdH1") {
-            basic.showIcon(IconNames.Angry)
-        } else if (receivedString == "6NCnBwkRBQ") {
-            basic.showIcon(IconNames.Asleep)
-        } else if (receivedString == "Sw6ILbQUcj") {
-            basic.showIcon(IconNames.Silly)
-        } else if (receivedString == "7loUKZq9rv") {
-            basic.showIcon(IconNames.Fabulous)
-        } else if (receivedString == "2nVj8GTRIm") {
-            basic.showIcon(IconNames.Surprised)
-        } else if (receivedString == "GLeQMn7dmT") {
-            basic.showIcon(IconNames.EigthNote)
-        } else if (receivedString == "n3QZAVpdPE") {
-            basic.showIcon(IconNames.Skull)
-        } else if (receivedString == "SSk3N6pBdh") {
+    if (interferencecheck == 0) {
+        if (receivedString == "connected") {
             basic.showIcon(IconNames.Yes)
-        } else if (receivedString == "BqbrStClPh") {
-            basic.showIcon(IconNames.No)
-        } else if (receivedString == "YPX55sqbhU") {
+            basic.pause(500)
             basic.showLeds(`
-                . # # # .
+                . . . . .
+                # # # # #
                 # . . . #
-                . . # # .
+                # # # # #
+                . . . # .
+                `)
+            music.playMelody("E G B C5 - - - - ", 165)
+            connectindicator = 1
+        } else if (receivedString == "connect") {
+            disableinterferencecheck = 1
+            radio.sendString("connected")
+            basic.showIcon(IconNames.Yes)
+            basic.pause(1000)
+            basic.showLeds(`
+                . . . . .
+                # # # # #
+                # . . . #
+                # # # # #
+                . . . # .
+                `)
+            music.playMelody("E G B C5 - - - - ", 165)
+            connectindicator = 1
+        } else if (connectindicator == 1) {
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                . . # . .
                 . . . . .
                 . . # . .
                 `)
+            music.playMelody("G A C5 - - - - - ", 190)
+            basic.pause(500)
+            if (receivedString == "O1Vt55XkxF") {
+                basic.showIcon(IconNames.Heart)
+            } else if (receivedString == "2iexReSuMD") {
+                basic.showIcon(IconNames.Happy)
+            } else if (receivedString == "aH8rn9PxmT") {
+                basic.showIcon(IconNames.Sad)
+            } else if (receivedString == "fKk4YXjdH1") {
+                basic.showIcon(IconNames.Angry)
+            } else if (receivedString == "6NCnBwkRBQ") {
+                basic.showIcon(IconNames.Asleep)
+            } else if (receivedString == "Sw6ILbQUcj") {
+                basic.showIcon(IconNames.Silly)
+            } else if (receivedString == "7loUKZq9rv") {
+                basic.showIcon(IconNames.Fabulous)
+            } else if (receivedString == "2nVj8GTRIm") {
+                basic.showIcon(IconNames.Surprised)
+            } else if (receivedString == "GLeQMn7dmT") {
+                basic.showIcon(IconNames.EigthNote)
+            } else if (receivedString == "n3QZAVpdPE") {
+                basic.showIcon(IconNames.Skull)
+            } else if (receivedString == "SSk3N6pBdh") {
+                basic.showIcon(IconNames.Yes)
+            } else if (receivedString == "BqbrStClPh") {
+                basic.showIcon(IconNames.No)
+            } else if (receivedString == "YPX55sqbhU") {
+                basic.showLeds(`
+                    . # # # .
+                    # . . . #
+                    . . # # .
+                    . . . . .
+                    . . # . .
+                    `)
+            } else {
+                music.playMelody("D C - - - - - - ", 220)
+                basic.showString("ERR 2")
+            }
         } else {
-            music.playMelody("D C - - - - - - ", 220)
-            basic.showString("ERR 2")
+            basic.showLeds(`
+                # . . . #
+                . # . # .
+                . . # . .
+                . # . # .
+                # . . . #
+                `)
+            basic.showString("ERR 1")
+            music.playMelody("C - - - - - - - ", 150)
         }
-    } else {
-        basic.showLeds(`
-            # . . . #
-            . # . # .
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
-        basic.showString("ERR 1")
-        music.playMelody("C - - - - - - - ", 150)
     }
 })
 input.onButtonPressed(Button.B, function () {
@@ -203,37 +205,61 @@ input.onButtonPressed(Button.B, function () {
     }
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    disableinterferencecheck = 1
-    radio.sendString("connect")
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        # . # . #
-        . . . . .
-        . . . . .
-        `)
+    basic.clearScreen()
+    if (interferencecheck == 0) {
+        radio.sendString("connect")
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . # . #
+            . . . . .
+            . . . . .
+            `)
+    }
 })
+let disableinterferencecheck = 0
 let connectindicator = 0
 let Text1 = 0
-let disableinterferencecheck = 0
+let interferencecheck = 0
 radio.setGroup(167)
 basic.pause(100)
-if (disableinterferencecheck == 0) {
-    basic.showLeds(`
-        . . . . .
-        . # # # .
-        . # . # .
-        . # # # .
-        . . . . .
-        `)
+interferencecheck = 1
+for (let index = 0; index < 3; index++) {
+    led.plot(1, 1)
+    basic.pause(370)
+    led.unplot(1, 1)
+    led.plot(2, 1)
+    basic.pause(370)
+    led.unplot(2, 1)
+    led.plot(3, 1)
+    basic.pause(370)
+    led.unplot(3, 1)
+    led.plot(3, 2)
+    basic.pause(370)
+    led.unplot(3, 2)
+    led.plot(3, 3)
+    basic.pause(370)
+    led.unplot(3, 3)
+    led.plot(2, 3)
+    basic.pause(370)
+    led.unplot(2, 3)
+    led.plot(1, 3)
+    basic.pause(370)
+    led.unplot(1, 3)
+    led.plot(1, 2)
+    basic.pause(370)
+    led.unplot(1, 2)
 }
-basic.pause(10000)
-if (disableinterferencecheck == 0) {
-    basic.showLeds(`
-        . . . . .
-        . # # # .
-        . # # # .
-        . # # # .
-        . . . . .
-        `)
-}
+basic.clearScreen()
+basic.pause(100)
+led.plot(0, 3)
+basic.pause(100)
+led.plot(1, 4)
+basic.pause(100)
+led.plot(2, 3)
+basic.pause(100)
+led.plot(3, 2)
+basic.pause(100)
+led.plot(4, 1)
+basic.pause(500)
+interferencecheck = 0
